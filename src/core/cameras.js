445064e78e39e8d4e837928042bf2502d0e39235
unknown <komogortev@users.noreply.github.com>
2022-04-19 13:13:14 -0400

    cleanup: cameras in constructor

--- src/core/cameras.js
+++ src/core/cameras.js
@@ -1,50 +1,47 @@
-import {  PerspectiveCamera } from 'three'
+import { PerspectiveCamera } from 'three'
 
 export const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 }
 
-export const camera = new PerspectiveCamera(
-  75, // FOV
-  window.innerWidth / window.innerHeight, // aspect ratio
-  0.1, // near clipping plane
-  1000 // far clipping plane
-)
-camera.position.set(0, 0, 100);
-camera.lookAt(0, 0, 0);
+// export const camera = new PerspectiveCamera(
+//   75, // FOV
+//   window.innerWidth / window.innerHeight, // aspect ratio
+//   0.1, // near clipping plane
+//   1000 // far clipping plane
+// )
+// camera.position.set(0, 0, 100);
+// camera.lookAt(0, 0, 0);
 
-export const turretCamera = new PerspectiveCamera(
-  75, // FOV
-  window.innerWidth / window.innerHeight, // aspect ratio
-  0.1, // near clipping plane
-  1000 // far clipping plane
-)
+// export const turretCamera = new PerspectiveCamera(
+//   75, // FOV
+//   window.innerWidth / window.innerHeight, // aspect ratio
+//   0.1, // near clipping plane
+//   1000 // far clipping plane
+// )
 
-export const golemCamera = new PerspectiveCamera(
-  75, // FOV
-  window.innerWidth / window.innerHeight, // aspect ratio
-  0.1, // near clipping plane
-  1000 // far clipping plane
-)
+// export const golemCamera = new PerspectiveCamera(
+//   75, // FOV
+//   window.innerWidth / window.innerHeight, // aspect ratio
+//   0.1, // near clipping plane
+//   1000 // far clipping plane
+// )
 
 
-export function makePerspectiveCamera(fov = 40) {
-  const aspect = 2 // the canvas default
-  const zNear = 0.1
-  const zFar = 1000
+function makePerspectiveCamera(fov = 40, aspect = 2, zNear = 0.1, zFar = 1000) {
   return new PerspectiveCamera(fov, aspect, zNear, zFar)
 }
 
 window.addEventListener('resize', () => {
   sizes.width = window.innerWidth
   sizes.height = window.innerHeight
-  camera.aspect = sizes.width / sizes.height
-  turretCamera.aspect = sizes.width / sizes.height
-  golemCamera.aspect = sizes.width / sizes.height
-  camera.updateProjectionMatrix()
-  turretCamera.updateProjectionMatrix()
-  golemCamera.updateProjectionMatrix()
+  // camera.aspect = sizes.width / sizes.height
+  // turretCamera.aspect = sizes.width / sizes.height
+  // golemCamera.aspect = sizes.width / sizes.height
+  // camera.updateProjectionMatrix()
+  // turretCamera.updateProjectionMatrix()
+  // golemCamera.updateProjectionMatrix()
 })
 
-export default camera
+export { makePerspectiveCamera }
